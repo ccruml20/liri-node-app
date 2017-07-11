a379bce4798c117a1c6dc017d3eafb4e3e5e0d48
ccruml20 <ccrumley1@gmail.com>
2017-07-11 12:06:09 -0600

    first push with get issues

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'VJl6L4SIOQSf4ueRA9EnObFy4',
+  consumer_secret: '70tVdbGlMFhpRN5CWbAYPqjAcTU6Xk4xyOawpkcfnB4n0KMko',
+  access_token_key: '837772123187785728-BrWTK6L8UqbuyqyOOAJiVXDvKColf4Y',
+  access_token_secret: 'NAyNUNK0n7tiuz9rSF67ZbUKehGRebSjidRF8F6JdMFYk'
+};
+
---
+++ liri.js
@@ -0,0 +1,16 @@
+
+var Twitter = require('twitter');
+var keys = require('./keys.js');
+
+
+
+	var client = new Twitter(keys);
+	var params = {screen_name: 'ccruml20'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			console.log(tweets);
+		}
+	})();
+
+
+
