a79532fecf8830053e2ed431c5254bd37a0d41d3
ccruml20 <ccrumley1@gmail.com>
2017-07-17 11:42:22 -0600

    Almost Done

--- keys.js
+++ keys.js
@@ -2,8 +2,14 @@ console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'VJl6L4SIOQSf4ueRA9EnObFy4',
-  consumer_secret: '70tVdbGlMFhpRN5CWbAYPqjAcTU6Xk4xyOawpkcfnB4n0KMko',
+  consumer_secret: '70tVdbGlMFhpRN5CWbAYPqjAcTU6Xk4xyOawpkcfnB4n0KMkoM',
   access_token_key: '837772123187785728-BrWTK6L8UqbuyqyOOAJiVXDvKColf4Y',
   access_token_secret: 'NAyNUNK0n7tiuz9rSF67ZbUKehGRebSjidRF8F6JdMFYk'
 };
 
+exports.spotifyKeys = {
+		id: '439ba3ba08b3423d9d53ffd4750c29e5',
+		secret: 'f647609a2d45412d838dca871fe59cbb'
+	};
+
+exports.movieKey = 'apikey=40e9cece';
--- liri.js
+++ liri.js
@@ -1,16 +1,69 @@
-
-var Twitter = require('twitter');
 var keys = require('./keys.js');
+var inputOne = process.argv[2];
+var inputTwo = process.argv[3];
+
+//Node Command Line Aguments For Calling Specific API Requests
+if (inputOne === 'my-tweets') {
+    twitterCall();
+} else if (inputOne === 'spotify-this-song') {
+    spotifyCall();
+} else if (inputOne === 'movie-this') {
+    movieDataCall();
+} else if (inputOne === 'do-what-it-says'){
+	var fs = require('./random.txt');
+};
+
+//Twitter Get Request And Return Object Function
+function twitterCall() {
+    var Twitter = require('twitter');
+    var client = new Twitter(keys.twitterKeys);
+    var params = { screen_name: 'ccruml20', count: "20" };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var key in tweets) {
+                console.log("===========", tweets[key].text);
+            }
+        }
+    });
+}
+
+//Spotify API Request and Return Object Function
+function spotifyCall() {
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(keys.spotifyKeys);
+    spotify.search({ type: 'track', query: `${inputTwo}`, limit: '1' }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var dataObj = data.tracks.items[0].album;
+        console.log(dataObj.name);
+        console.log(dataObj.external_urls.spotify);
+        console.log(dataObj.artists[0].name);
+    });
+};
 
 
-
-	var client = new Twitter(keys);
-	var params = {screen_name: 'ccruml20'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		if (!error) {
-			console.log(tweets);
-		}
-	})();
-
-
-
+//Movie Database API Request and Return Object Function
+function movieDataCall() {
+    var request = require('request');
+    request(`http://www.omdbapi.com/?t=${inputTwo}&tomatoes=true&${keys.movieKey}`, function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred 
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+        // console.log('body:', body); // Print the HTML for the Google homepage. 
+        var bodyObj = JSON.parse(body);
+        console.log(bodyObj.Ratings);
+        for (var key in bodyObj.Ratings) {
+            console.log(key, "========", bodyObj.Ratings[key])
+        };
+        console.log(`
+        	Title = ${bodyObj.Title}
+        	Year = ${bodyObj.Year}
+        	Rating = ${bodyObj.imdbRated}
+        	Country = ${bodyObj.Country}
+        	Language = ${bodyObj.Language}
+        	Plot = ${bodyObj.Plot}
+        	Actors = ${bodyObj.Actors} 
+        	Rotten Tomatoes Rating = ${bodyObj.Rating} 
+        	`);
+    });
+};
